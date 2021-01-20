030f2009ec03f7b4ad032b715f1f63730877cde6
Сергей Киринцев <kserg80@gmail.com>
2021-01-20 21:10:43 +0300

    Задача 2: «FizzBuzz»

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -1,3 +1,5 @@
+// https://youtu.be/TWmmfDvcYO0
+
 /*
  * Задача 2: «FizzBuzz»
  *
@@ -7,11 +9,17 @@
  * • кратные трём — на fizz;
  * • кратные пяти — на buzz;
  * • кратные и трём, и пяти одновременно — на fizzbuzz.
- * 
-*/
+ *
+ */
+
+const gen = (n, w) => (num) => (num % n === 0 ? w : "");
+const fizz = gen(3, "fizz");
+const buzz = gen(5, "buzz");
 
 function fizzBuzz(num) {
-    // Напишите код здесь
+  [...Array(num).keys()]
+    .map((i) => i + 1)
+    .forEach((i) => console.log(fizz(i) + buzz(i) || i));
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
