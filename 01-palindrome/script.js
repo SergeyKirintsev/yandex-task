5c4d0115dd8795cafd2ab62991cc5d5471d07d28
Сергей Киринцев <kserg80@gmail.com>
2021-01-20 20:42:45 +0300

    01-palindrome

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -7,26 +7,58 @@
  *
  * Напишите функцию palindrome(str), принимающую как аргумент строку.
  * Функция должна вернуть true, если строка — палиндром, и false, если нет.
- * 
+ *
  * Считайте, что на вход всегда передаётся слово: то есть знаков препинания
  * и пробелов в аргументе быть не может.
- * 
-*/
+ *
+ */
+
+const startHalfStr = (str) => str.slice(0, str.length / 2);
+
+const endHalfStr = (str) => {
+  if (str.length % 2 == 0) return str.slice(str.length / 2, str.length);
+  return str.slice(str.length / 2 + 1, str.length);
+};
+
+const reversStr = (str) => str.split("").reverse().join("");
+
+const onlyAlhabet = (str) => {
+  let alhabetEn = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let alhabetRu =
+    "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+  let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (alhabetEn.includes(letter) || alhabetRu.includes(letter)) {
+      newStr += letter;
+    }
+  }
+
+  return newStr;
+};
 
 function palindrome(str) {
-    // Напишите код здесь
+  let letters = onlyAlhabet(str);
+  return (
+    startHalfStr(letters).toUpperCase() ===
+    reversStr(endHalfStr(letters)).toUpperCase()
+  );
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(palindrome('топот')); // должно быть true
-console.log(palindrome('Saippuakivikauppias')); // true
-console.log(palindrome('привет')); // false
+// console.log(palindrome("топот")); // должно быть true
+// console.log(palindrome("Saippuakivikauppias")); // true
+// console.log(palindrome("привет")); // false
+
+// console.log(palindrome("О, лета тело!")); // true
 
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
- * 
+ *
  * palindrome('О, лета тело!'); // true
- * 
-*/
+ *
+ */
