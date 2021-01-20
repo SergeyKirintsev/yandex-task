ab8653ea016be2fcd14656d6bd55f6ac83190a7a
Сергей Киринцев <kserg80@gmail.com>
2021-01-20 22:40:16 +0300

    Задача 4: «С большой буквы»

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -4,12 +4,20 @@
  * Напишите функцию capitalize(str). Функция должна возвращать новую
  * строку каждое слово в которой начинается с прописной буквы.
  *
-*/
+ */
 
 function capitalize(str) {
-    // Напишите код здесь
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    if (str[i - 1] === " " || str[i - 1] === undefined)
+      newStr += element.toUpperCase();
+    else newStr += element;
+  }
+  return newStr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(capitalize('молодость всё простит')); // "Молодость Всё Простит"
+//  console.log(capitalize("молодость всё  простит")); // "Молодость Всё Простит"
+// console.log(capitalize("молодость "));
