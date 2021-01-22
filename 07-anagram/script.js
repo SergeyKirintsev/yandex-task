d3e8cfd31d551c510cb4d0e27a809bec4de211ba
SergeyKirintsev <49301786+SergeyKirintsev@users.noreply.github.com>
2021-01-22 15:19:00 +0300

    Задача 7 Анаграмма

--- 07-anagram/script.js
+++ 07-anagram/script.js
@@ -9,7 +9,23 @@
 */
 
 function anagram(str1, str2) {
-    // Напишите код здесь
+    if (str1.length !=  str2.length) return false;
+
+    let s1 = str1.toLowerCase();
+    let s2 = str2.toLowerCase();
+
+    if (s1 === s2) return false;
+
+    s1 = str1.toLowerCase().split('').sort();
+    s2 = str2.toLowerCase().split('').sort();
+
+    let setStr1 = new Set(s1);
+    let setStr2 = new Set(s2);
+
+    s1 = Array.from(setStr1).join('');
+    s2 = Array.from(setStr2).join('');
+
+    return s1 === s2;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
